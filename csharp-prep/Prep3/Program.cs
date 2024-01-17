879186eb395947d79bf6d6e7b64ff1b4b57a2f40
Grant Jacobsen <grantljacobsen@gmail.com>
2024-01-17 16:05:52 -0700

    class 1/17

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rnd = new Random();
+        int magicNumber = rnd.Next(1,101);
+        int guess;
+        int count = 0;
+
+        Console.WriteLine("Guess a number between 1-100");
+        do {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            count++;
+
+            if(guess>magicNumber){
+                Console.WriteLine("Lower");
+            }
+            else if(guess<magicNumber){
+                Console.WriteLine("Higher");
+            }
+
+        } while(guess != magicNumber);
+
+        Console.WriteLine("You guessed it in {0} guesses!",count);
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,22 @@
 using System;
-
+using System.Diagnostics.CodeAnalysis;
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    static void Main(string[] args){
+        List<int> numbers = new List<int>();
+        int entry;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            Console.Write("Enter number: ");
+            entry = int.Parse(Console.ReadLine());
+            numbers.Add(entry);
+
+        } while (entry != 0);
+        int sum = numbers.Sum();
+        double average = Queryable.Average(numbers.AsQueryable());
+        
+        Console.Write("The sum is: {0}/nThe average is: {1}/nThe largest number is:{2}",sum,average,largest);
     }
 }
