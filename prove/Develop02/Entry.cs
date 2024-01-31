1cc0e3e8d17d50b0635bde4af0945d82984ded2e
Grant Jacobsen <grantljacobsen@gmail.com>
2024-01-31 12:10:31 -0700

    Taco Bell

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -3,31 +3,42 @@ using System.Collections.Generic;
 
 namespace Develop2;
 
-class Entry
+public class Entry
 {
-    public string Response;
-    public string Prompt;
-    public string Date;
+    public string response;
+    public string prompt;
+    public DateTime date;
     public List<string> prompts = new List<string>{
     "Who was the most interesting person I interacted with today?",
     "What was the best part of my day?",
     "How did I see the hand of the Lord in my life today?",
     "What was the strongest emotion I felt today?",
     "If I had one thing I could do over today, what would it be?"};
-    public Entry (string response, string date)
+    public Entry()
     {
-        Response  = response;
-        Prompt = prompts[new Random().Next(prompts.Count)];
-        Date = date;
+        
+    }
+    public Entry (string response, DateTime date,string prompt)
+    {
+        this.date = date;
+        this.response = response;
+        this.prompt = prompt;
     }
     
     public Entry(string import)
     {
-
+        
     }
-    
+    public Entry WriteNewEntry(DateTime date)
+    {
+        prompt = prompts[new Random().Next(prompts.Count)];
+        Console.WriteLine(prompt);
+        response = Console.ReadLine();
+        Entry entry = new(response,date,prompt);
+        return entry;
+    }
     public void Display()
     {
-        Console.WriteLine($"{Date}\n{Prompt}\n{Response}");
+        Console.WriteLine($"{date}\n{prompt}\n{response}");
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -9,20 +9,15 @@ class Program
 
         while (true)
         {
-            Console.WriteLine("Menu:");
-            Console.WriteLine("1. Write a new entry");
-            Console.WriteLine("2. Display the journal");
-            Console.WriteLine("3. Save the journal to a file");
-            Console.WriteLine("4. Load the journal from a file");
-            Console.WriteLine("5. Exit");
-
+            ShowMenu();
             Console.Write("Enter your choice (1-5): ", 1, 5);
-            int choice = Prase(Console.ReadLine();)
+            int choice = Int32.Parse(Console.ReadLine());
 
             switch (choice)
             {
                 case 1:
-                    journal.WriteNewEntry();
+                    Entry entry = new();
+                    entry.WriteNewEntry(GetDate());
                     break;
 
                 case 2:
@@ -38,12 +33,27 @@ class Program
                     break;
 
                 case 5:
+                    Console.Clear();
                     Environment.Exit(0);
                     break;
             }
         }
     }
 
+    static void ShowMenu()
+    {
+        Console.WriteLine("Menu:");
+        Console.WriteLine("1. Write a new entry");
+        Console.WriteLine("2. Display the journal");
+        Console.WriteLine("3. Save the journal to a file");
+        Console.WriteLine("4. Load the journal from a file");
+        Console.WriteLine("5. Exit");
+    }
+    static DateTime GetDate()
+    {
+        DateTime date = DateTime.Today;
+        return date;
+    }
     public string GetPrompt()
     {
         return "";
