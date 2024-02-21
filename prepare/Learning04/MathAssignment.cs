f64b8f95270948a16b584638093a415bdbc2429b
Grant Jacobsen <grantljacobsen@gmail.com>
2024-02-21 11:03:35 -0700

    Learning04

--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -3,11 +3,16 @@ public class MathAssignment : Assignment
 {
     private string _textbookSection;
     private string _problems;
+    //     private string _studentName;
+    //     private string _topic;
+
 
     public MathAssignment(string name, string topic, string textbookSection, string problems) : base(name,topic)
     {
         _textbookSection = textbookSection;
         _problems = problems;
+        // _studentName = name;
+        // _topic = topic;
     }
     public string GetHomeworkList()
     {
