82362021de4f1f03bd51074307985150ac812b36
Grant Jacobsen <grantljacobsen@gmail.com>
2024-02-21 11:08:18 -0700

    Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -6,5 +6,13 @@ class Program
     {
         Assignment assignment = new("Samuel Bennett", "Multiplication");
         assignment.GetSummary();
+
+        MathAssignment mathAssignment = new("Roberto Rodriguez","Fractions","7.3", "8-19");
+        mathAssignment.GetSummary();
+        mathAssignment.GetHomeworkList();
+
+        WritingAssignment writingAssignment = new("Mary Waters","European History","The Causes of World War II by Mary Waters");
+        writingAssignment.GetSummary();
+        writingAssignment.GetWritingInformation();
     }
 }
