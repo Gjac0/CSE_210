6cfc369c62e9b71fcee4f3d9f607d1418fb663ca
Grant Jacobsen <grantljacobsen@gmail.com>
2024-02-21 11:14:25 -0700

    Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,14 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Assignment assignment = new("Samuel Bennett", "Multiplication");
-        assignment.GetSummary();
+        Console.WriteLine($"{assignment.GetSummary()}");
 
         MathAssignment mathAssignment = new("Roberto Rodriguez","Fractions","7.3", "8-19");
-        mathAssignment.GetSummary();
-        mathAssignment.GetHomeworkList();
+        Console.WriteLine($"{mathAssignment.GetSummary()}\n{mathAssignment.GetHomeworkList()}");
 
         WritingAssignment writingAssignment = new("Mary Waters","European History","The Causes of World War II by Mary Waters");
-        writingAssignment.GetSummary();
-        writingAssignment.GetWritingInformation();
+        Console.WriteLine($"{writingAssignment.GetSummary()}\n{writingAssignment.GetWritingInformation()}");
     }
 }
