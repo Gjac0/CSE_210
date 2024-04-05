380cda15989dcad11b90f733ab533c844781509f
Grant Jacobsen <grantljacobsen@gmail.com>
2024-04-04 18:40:02 -0600

    :)

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -5,10 +5,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Activity> activities = new();
-        activities.Add(new Running("13 May 2024", 30, 3.0));
-        activities.Add(new Cycling("14 May 2024", 30, 6.0));
-        activities.Add(new Swimming("20 May 2024", 30, 20));
+        List<Activity> activities = new(){
+        new Running("13 May 2024", 30, 3.0),
+        new Cycling("14 May 2024", 30, 6.0),
+        new Swimming("20 May 2024", 30, 20)};
 
         // Displaying summary information
         foreach (var activity in activities)
